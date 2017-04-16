33c5c6215ffd86eb83e54a304d4ce3465532944f
Jessica Kerr <jessitron@gmail.com>
2017-04-15 22:16:45 -0500

    changes

--- .atomist/handlers/command/ListMyIssues.ts
+++ .atomist/handlers/command/ListMyIssues.ts
@@ -55,27 +55,62 @@ class ReceiveMyIssues implements HandleResponse<any> {
 
         let count = result.total_count;
 
+        //TODO: in the search string, ignore ones closed a long time ago so we don't get to many.
+        let closedOnes = result.items.filter(item => this.not_long_ago(item.closed_at));
+        let openOnes = result.items.filter(item => !item.closed_at);
+
         let information = result.items.map(item => {
             let type = this.issueOrPR(item);
             let repo = this.issueRepo(item);
-            let labels = item.labels.map(label => `:${label.name}:`).join(" ");
+            let labels = item.labels.map(label => `:${label.name.replace(":", "-")}:`).join(" ");
 
             return {
                 "mrkdwn_in": ["text"],
+                "color": "#3D9900", 
                 "title": `<${item.html_url}|${repo} ${type} #${item.number}: ${item.title}>`,
-                "text": `${labels} created ${this.timeSince(item.created_at)}, updated ${this.timeSince(item.updated_at)}, closed ${this.timeSince(item.closed_at)}`,
+                "text": `${labels} created ${this.timeSince(item.created_at)}, updated ${this.timeSince(item.updated_at)}`,
+                "fallback": item.html_url
+            };
+        });
+
+        let closedInformation = result.items.map(item => {
+            let type = this.issueOrPR(item);
+            let repo = this.issueRepo(item);
+            let labels = item.labels.map(label => `:${label.name.replace(":", "-")}:`).join(" ");
+
+            return {
+                "mrkdwn_in": ["text"],
+                "color": "#0066FF",
+                "title": `<${item.html_url}|${repo} ${type} #${item.number}: ${item.title}>`,
+                "text": `${labels} created ${this.timeSince(item.created_at)}, closed ${this.timeSince(item.closed_at)}`,
                 "fallback": item.html_url
             };
         });
 
         let slack = {
-            text: `You have ${count} things going`,
-            attachments: information
+            text: `You have ${information.length} things going`,
+            attachments: closedInformation.concat(information)
         };
 
         let plan = Plan.ofMessage(new ResponseMessage(JSON.stringify(slack), MessageMimeTypes.SLACK_JSON));
 
         return plan;
+    }
+
+    private not_long_ago(dateString) {
+        if (dateString == null) {
+            return false;
+        }
+        let recent = 86400; // a day
+        if (new Date().getDay() == 1) {
+            // it is Monday
+            recent = recent * 3; // think back to Friday
+        }
+        let then = Date.parse(dateString)
+        let now = new Date().getTime();
+        let secondsPast = (now - then) / 1000;
+
+        return secondsPast < recent;
     }
 
     private issueRepo(item) {
@@ -85,7 +120,7 @@ class ReceiveMyIssues implements HandleResponse<any> {
     }
 
     private issueOrPR(item) {
-        if (item.url.indexOf("/issues/") > 0) {
+        if (item.html_url.indexOf("/issues/") > 0) {
             return "issue";
         }
         return "pr";
@@ -99,7 +134,7 @@ class ReceiveMyIssues implements HandleResponse<any> {
         let now = new Date().getTime();
         let secondsPast = (now - then) / 1000;
         if (secondsPast < 60) {
-            return `${secondsPast}s ago`;
+            return `${Math.round(secondsPast)}s ago`;
         }
         if (secondsPast < 3600) {
             return `${Math.round(secondsPast / 60)}m ago`;
