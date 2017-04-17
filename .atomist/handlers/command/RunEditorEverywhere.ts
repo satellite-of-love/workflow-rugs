39e4a6fef18f8b9d1e837fc583c9bc2f2d260329
Jessica Kerr <jessitron@gmail.com>
2017-04-17 16:14:55 -0400

    changes

--- .atomist/handlers/command/RunEditorEverywhere.ts
+++ .atomist/handlers/command/RunEditorEverywhere.ts
@@ -12,12 +12,18 @@ import { Repo } from '@atomist/cortex/Repo';
 export class RunEditorEverywhere implements HandleCommand {
 
     @Parameter({
-        displayName: "editor coordinate",
-        description: "the (parameterless) editor to run, like satellite-of-love:atomist-k8-specs:ChangeMemoryRequirements",
-        pattern: Pattern.any,
-        validInput: "a description of the valid input",
-        minLength: 1,
-        maxLength: 100
+        displayName: "rug archive holding this generator",
+        description: "workflow-rugs or rest-service-generator",
+        pattern: Pattern.project_name,
+        validInput: "workflow-rugs or rest-service-generator"
+    })
+    rugArchive: string;
+
+    @Parameter({
+        displayName: "editor name",
+        description: "the (parameterless) editor to run, like ChangeMemoryRequirements",
+        pattern: Pattern.project_name,
+        validInput: "a description of the valid input"
     })
     editorName: string;
 
@@ -28,8 +34,16 @@ export class RunEditorEverywhere implements HandleCommand {
         // match (ct: ChatTeam { name: "satellite-of-love" } ) - [:OWNS] - (gh: Org) - [:HAS] - (r: Repo) return r
 
         pxe.with<Repo>(command.contextRoot, "/ChatTeam()/Org()/Repo()", r => {
+            plan.add(new ResponseMessage(`Running ${this.editorName} on ${r.name}`))
             plan.add({
-                instruction: { kind: "edit", name: this.editorName, project: r.name },
+                instruction: {
+                    kind: "edit", name:
+                        {
+                            name: this.editorName,
+                            group: "satellite-of-love",
+                            artifact: this.rugArchive
+                        }, project: r.name
+                },
                 onSuccess: new ResponseMessage("Created PR on " + r.name),
                 onError: new ResponseMessage("Failed to edit " + r.name)
             });
