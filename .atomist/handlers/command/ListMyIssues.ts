8af1ba68721cd0d95be6364d96bcfbf3e0cc45f8
Jessica Kerr <jessitron@gmail.com>
2017-04-18 11:09:50 -0400

    changes

--- .atomist/handlers/command/ListMyIssues.ts
+++ .atomist/handlers/command/ListMyIssues.ts
@@ -64,13 +64,17 @@ class ReceiveMyIssues implements HandleResponse<any> {
             let repo = this.issueRepo(item);
             let labels = item.labels.map(label => `:${label.name.replace(":", "-")}:`).join(" ");
 
-            return {
+            let slack: any = {
                 "mrkdwn_in": ["text"],
-                "color": "#3D9900", 
+                "color": "#3D9900",
                 "title": `<${item.html_url}|${repo} ${type} #${item.number}: ${item.title}>`,
                 "text": `${labels} created ${this.timeSince(item.created_at)}, updated ${this.timeSince(item.updated_at)}`,
                 "fallback": item.html_url
             };
+            if (this.not_long_ago(item.created_at)) {
+                slack.thumb_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Sol.svg/256px-Sol.svg.png"
+            }
+            return slack;
         });
 
         let closedInformation = closedOnes.map(item => {
@@ -83,7 +87,9 @@ class ReceiveMyIssues implements HandleResponse<any> {
                 "color": "#0066FF",
                 "title": `<${item.html_url}|${repo} ${type} #${item.number}: ${item.title}>`,
                 "text": `${labels} created ${this.timeSince(item.created_at)}, closed ${this.timeSince(item.closed_at)}`,
-                "fallback": item.html_url
+                "fallback": item.html_url,
+                "thumb_url": "https://upload.wikimedia.org/wikipedia/commons/9/91/Checked_icon.png"
+
             };
         });
 
@@ -126,7 +132,7 @@ class ReceiveMyIssues implements HandleResponse<any> {
         return "pr";
     }
 
-    private timeSince(dateString : string) {
+    private timeSince(dateString: string) {
         if (dateString == null) {
             return "never";
         }
@@ -146,7 +152,7 @@ class ReceiveMyIssues implements HandleResponse<any> {
             return `${Math.round(secondsPast / 86400)}d ago`;
         }
         else {
-            return dateString.substr(0,10);
+            return dateString.substr(0, 10);
         }
     }
 }
