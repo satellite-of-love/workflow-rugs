eb76d37be202719bba4c9ea8fe1de34d289fa547
Jessica Kerr <jessitron@gmail.com>
2017-04-17 21:21:53 -0400

    changes

--- .atomist/handlers/command/ListMyIssues.ts
+++ .atomist/handlers/command/ListMyIssues.ts
@@ -2,6 +2,8 @@ import { HandleCommand, MappedParameters, MessageMimeTypes, Response, HandleResp
 import { EventHandler, ResponseHandler, ParseJson, CommandHandler, Secrets, MappedParameter, Parameter, Tags, Intent } from '@atomist/rug/operations/Decorators'
 import { Pattern } from '@atomist/rug/operations/RugOperation';
 import * as PlanUtils from '@atomist/rugs/operations/PlanUtils';
+import { ChatTeam } from '@atomist/cortex/ChatTeam';
+import { GitHubId } from '@atomist/cortex/GitHubId';
 
 /**
  * A sample Rug TypeScript command handler.
@@ -12,13 +14,23 @@ import * as PlanUtils from '@atomist/rugs/operations/PlanUtils';
 @Secrets("github://user_token?scopes=repo")
 class ListMyIssues implements HandleCommand {
 
-    // TODO: accept user; use path expression to get GitHub login.
+    @MappedParameter(MappedParameters.SLACK_USER)
+    user: string;
 
     handle(command: HandlerContext): Plan {
         let plan = new Plan();
 
-        let user = "jessitron"
-        let org = "satellite-of-love"
+        let pxe = command.pathExpressionEngine;
+
+        let match = pxe.scalar<ChatTeam, GitHubId>(command.contextRoot as ChatTeam,
+            `/members::ChatId()[@id='${this.user}']/person::Person()/gitHubId::GitHubId()`);
+
+        let user = "jessitron";
+        if (match && match.login) {
+            console.log(match.login);
+            user = match.login;
+        }
+        let org = "satellite-of-love";
 
         const base = `https://api.github.com/search/issues`;
 
@@ -66,7 +78,7 @@ class ReceiveMyIssues implements HandleResponse<any> {
 
             return {
                 "mrkdwn_in": ["text"],
-                "color": "#3D9900", 
+                "color": "#3D9900",
                 "title": `<${item.html_url}|${repo} ${type} #${item.number}: ${item.title}>`,
                 "text": `${labels} created ${this.timeSince(item.created_at)}, updated ${this.timeSince(item.updated_at)}`,
                 "fallback": item.html_url
@@ -126,7 +138,7 @@ class ReceiveMyIssues implements HandleResponse<any> {
         return "pr";
     }
 
-    private timeSince(dateString : string) {
+    private timeSince(dateString: string) {
         if (dateString == null) {
             return "never";
         }
@@ -146,7 +158,7 @@ class ReceiveMyIssues implements HandleResponse<any> {
             return `${Math.round(secondsPast / 86400)}d ago`;
         }
         else {
-            return dateString.substr(0,10);
+            return dateString.substr(0, 10);
         }
     }
 }
