f36eeb9163ed37b5bcb53988d5eb2edbb046835c
Jessica Kerr <jessitron@gmail.com>
2017-04-24 15:34:54 -0500

    adding response handler

--- .atomist/handlers/command/MoveIssue.ts
+++ .atomist/handlers/command/MoveIssue.ts
@@ -1,30 +1,83 @@
-import { HandleCommand, HandlerContext, ResponseMessage, Plan } from '@atomist/rug/operations/Handlers';
-import { CommandHandler, Parameter, Tags, Intent } from '@atomist/rug/operations/Decorators';
+import { HandleCommand, HandlerContext, MappedParameters, Response, HandleResponse, ResponseMessage, Plan , Respondable} from '@atomist/rug/operations/Handlers';
+import { CommandHandler, ResponseHandler, Parameter, MappedParameter, Tags, Intent } from '@atomist/rug/operations/Decorators';
 import { Pattern } from '@atomist/rug/operations/RugOperation';
+import * as CommonHandlers from '@atomist/rugs/operations/CommonHandlers';
 
 /**
  * A move an issue from one repo to another.
  */
 @CommandHandler("MoveIssue", "move an issue from one repo to another")
-@Tags("documentation")
+@Tags("issue", "satellite-of-love", "workflow")
 @Intent("move issue")
 export class MoveIssue implements HandleCommand {
 
+    @MappedParameter(MappedParameters.GITHUB_REPOSITORY)
+    fromRepo: string;
+
+    @MappedParameter(MappedParameters.GITHUB_REPO_OWNER)
+    fromOrg: string;
+
     @Parameter({
-        displayName: "Some Input",
-        description: "example of how to specify a parameter using decorators",
+        displayName: "To org/repo",
+        description: "destination for the issue. org/repo",
         pattern: Pattern.any,
-        validInput: "a description of the valid input",
-        minLength: 1,
-        maxLength: 100,
-        required: false
+        validInput: "org/repo"
     })
-    inputParameter: string = "default value";
+    toOrgRepo: string;
+
+
+    @Parameter({
+        displayName: "Issue number",
+        description: "issue number",
+        pattern: Pattern.any
+    })
+    issue: string;
 
     handle(command: HandlerContext): Plan {
-        let message = new ResponseMessage(`Successfully ran MoveIssue: ${this.inputParameter}`);
-        return Plan.ofMessage(message);
+        let plan = new Plan();
+        let message = new ResponseMessage(`Moving issue: ${this.fromOrg}/${this.fromRepo}#${this.issue} to ${this.toOrgRepo}`);
+        plan.add(message);
+
+        const url = `https://api.github.com/repos/${this.fromOrg}/${this.fromRepo}/issues/${this.issue}`;
+        
+        let retrieveIssueInstruction: Respondable<any> = {
+            instruction: {
+                kind: "execute",
+                name: "http",
+                parameters: {
+                    url: url,
+                    method: "get",
+                    config: {
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Authorization": `token #{github://user_token?scopes=repo}`,
+                        },
+                    }
+                }
+            }
+            ,
+            onSuccess: { kind: "respond", name: "ReceiveIssueToMove", parameters: { } }
+        };
+        CommonHandlers.handleErrors(retrieveIssueInstruction, { msg: "The request to GitHub failed" });
+        plan.add(retrieveIssueInstruction);
+      
+
+        return plan;
     }
 }
 
+
+@ResponseHandler("ReceiveIssueToMove", "step 2 in MoveIssue")
+class ReceiveIssueToMove implements HandleResponse<any> {
+    @Parameter({ pattern: Pattern.any })
+    queryString: string;
+
+    handle(response: Response<any> ): Plan {
+        let plan = new Plan();
+        let result = JSON.parse(response.body)
+
+        return plan;
+    }}
+
+export const receiveIssueToMove = new ReceiveIssueToMove();
 export const moveIssue = new MoveIssue();
