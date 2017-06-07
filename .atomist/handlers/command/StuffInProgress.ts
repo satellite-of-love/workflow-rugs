590a0144a5ea7faab164ead65e4ea727fab06349
Jessica Kerr <jessitron@gmail.com>
2017-06-07 18:21:07 -0500

    changes

--- .atomist/handlers/command/StuffInProgress.ts
+++ .atomist/handlers/command/StuffInProgress.ts
@@ -24,9 +24,6 @@ import { toEmoji } from "./SlackEmoji";
 @Secrets("github://user_token?scopes=repo")
 class StuffInProgress implements HandleCommand {
 
-    @MappedParameter(MappedParameters.SLACK_CHANNEL)
-    channel: string;
-
     // TODO: accept user; use path expression to get GitHub login.
 
     public handle(command: HandlerContext): CommandPlan {
@@ -189,12 +186,7 @@ function closeInstruction(item): SlackMessages.IdentifiableInstruction & Identif
     const instr: Identifiable<"command"> = {
         instruction: {
             kind: "command",
-            name: {
-                name: "CloseGitHubIssue",
-                group: "atomist",
-                artifact: "github-rugs",
-            },
-            parameters: { issue: item.number },
+            name: "Dammit"
         }
     }
     const identifier: SlackMessages.IdentifiableInstruction = {
